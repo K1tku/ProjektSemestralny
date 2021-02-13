d9eb3be54859705d1218c1ad91684128b4e61884
Bartłomiej Kot <bartlomiej.kot@microsoft.wsei.edu.pl>
2021-02-13 19:01:18 +0100

    Dodanie okien wyboru oraz okien tabel

---
+++ Okna/OknoWyboru.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektSemestralny
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoWyboru.xaml
+    /// </summary>
+    public partial class OknoWyboru : Window
+    {
+        public OknoWyboru()
+        {
+            InitializeComponent();
+        }
+
+        private void Pracownicy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Gry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Wypozyczenia_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Klienci_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Zwroty_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
