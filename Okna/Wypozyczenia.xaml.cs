26c89013a6a1ecb3202495186000108b57958d3c
Bartłomiej Kot <bartlomiej.kot@microsoft.wsei.edu.pl>
2021-02-13 22:44:04 +0100

    Dodanie do tabeli "Wypozyczenia" mozliwosci modyfikacji tabeli

--- Okna/Wypozyczenia.xaml.cs
+++ Okna/Wypozyczenia.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace ProjektSemestralny
 {
@@ -19,6 +21,8 @@ namespace ProjektSemestralny
     /// </summary>
     public partial class Wypozyczenia : Window
     {
+        public String connection_String = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\C#\Projekty\ProjektSemestralny\Database1.mdf;Integrated Security=True";
+        public SqlConnection connection;
         public Wypozyczenia()
         {
             InitializeComponent();
@@ -26,17 +30,80 @@ namespace ProjektSemestralny
 
         private void dodaj_Click(object sender, RoutedEventArgs e)
         {
-
+            string Query = "insert into Wypozyczenia (ID_wypozyczenia, ID_Gry, ID_pracownika, ID_klienta, Data_wypozyczenia) values('" + this.iD_wypozyczeniaTextBox.Text + "','" + this.iD_GryTextBox.Text + "','" + this.iD_pracownikaTextBox.Text + "','" + this.iD_klientaTextBox.Text + "','" + this.iD_wypozyczeniaTextBox.Text + "');";
+            SqlConnection conDataBase = new SqlConnection(connection_String);
+            SqlCommand cmdDataBase = new SqlCommand(Query, conDataBase);
+            SqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                System.Windows.MessageBox.Show("Zapisano");
+                while (myReader.Read()) { }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            updateDataGrid();
         }
 
         private void uaktualnij_Click(object sender, RoutedEventArgs e)
         {
-
+            string Query = "update Wypozyczenia set ID_wypozyczenia='" + this.iD_wypozyczeniaTextBox.Text + "',ID_Gry='" + this.iD_GryTextBox.Text + "',ID_pracownika='" + this.iD_pracownikaTextBox.Text + "',ID_klienta='" + this.iD_klientaTextBox.Text + "',ID_wypozyczenia='" + this.iD_wypozyczeniaTextBox.Text + "'where ID_wypozyczenia='" + this.iD_wypozyczeniaTextBox.Text + "';";
+            SqlConnection conDataBase = new SqlConnection(connection_String);
+            SqlCommand cmdDataBase = new SqlCommand(Query, conDataBase);
+            SqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                System.Windows.MessageBox.Show("Uaktualniono");
+                while (myReader.Read()) { }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            updateDataGrid();
         }
 
         private void Usun_Click(object sender, RoutedEventArgs e)
         {
+            string Query = "delete from Wypozyczenia where ID_wypozyczenia='" + this.iD_wypozyczeniaTextBox.Text + "';";
+            SqlConnection conDataBase = new SqlConnection(connection_String);
+            SqlCommand cmdDataBase = new SqlCommand(Query, conDataBase);
+            SqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                System.Windows.MessageBox.Show("Zapisano");
+                while (myReader.Read()) { }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            updateDataGrid();
+        }
 
+        private void DataGridWypozyczenia_Loaded(object sender, RoutedEventArgs e)
+        {
+            updateDataGrid();
+        }
+        private void updateDataGrid()
+        {
+            connection = new SqlConnection(connection_String); connection = new SqlConnection(connection_String);
+            connection.Open();
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT ID_wypozyczenia, ID_Gry, ID_pracownika, ID_klienta, Data_wypozyczenia from dbo.Wypozyczenia";
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            DataGridWypozyczenia.ItemsSource = dt.DefaultView;
+            dr.Close();
         }
     }
 }
